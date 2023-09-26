7bf1f390c98fbbca6331fb47d6031f0fa9f00961
Chandna, Samay <schandna3@gatech.edu>
2023-09-26 08:03:16 -0400

    Created helper functions to make code readable Changed pins to guarantee support of serial RxTx & appendageID

--- mainBody.ts
+++ mainBody.ts
@@ -1,12 +1,12 @@
 // main body micro:bit module that extends micro:bit
 
 // pins used
-// 0, 1, 2, 3, 4 : analogIn for appendage ID
-// 5 : battery pin
-// 10, 11, 12, 13, 14 RX for appendage
-// 15, 16, 17, 18, 19 TX for appendage
+// 5, 6, 7, 9 : analogIn for appendage ID
+// 4 : battery pin
+// 0, 1, 2, 8,     RX for appendage
+// 12, 13, 14, 15, TX for appendage
 // (RX, TX) pairs
-// (10, 15), (11, 16), (12, 17), (13, 18), (14, 19)
+// (0, 12), (1, 13), (2, 14), (8, 15),
 
 class mainBody extends microBit {
     // one-hot vector of which appendages are connected
@@ -19,7 +19,7 @@ class mainBody extends microBit {
         this.appendagesConnected = [false, false, false, false, false];
         // assume battery is connected to pin 5 for votage reading
         // assuming 3.3V is max current, can add resistor and voltage divide
-        this.batteryRemaining = pins.analogReadPin(AnalogPin.P5) / 1023;
+        this.batteryRemaining = pins.analogReadPin(AnalogPin.P4) / 1023;
     }
     // create a thread for each module connected
     readAppendages() {
@@ -38,36 +38,16 @@ class mainBody extends microBit {
     // appendageID 0, analogIn 200
     readKaraoke() {
         while (true) {
-            let dataRX: number = 0;
-            let dataTX: number = 0;
-            if (pins.analogReadPin(AnalogPin.P0) == 200) {
-                dataRX = 10;
-                dataTX = 15;
-            }
-            else if (pins.analogReadPin(AnalogPin.P1) == 200) {
-                dataRX = 11;
-                dataTX = 16;
-            }
-            else if (pins.analogReadPin(AnalogPin.P2) == 200) {
-                dataRX = 12;
-                dataTX = 17;
-            }
-            else if (pins.analogReadPin(AnalogPin.P3) == 200) {
-                dataRX = 13;
-                dataTX = 18;
-            }
-            else if (pins.analogReadPin(AnalogPin.P4) == 200) {
-                dataRX = 14;
-                dataTX = 19;
-            }
-            if (dataRX || dataTX) {
+            let [dataRX, dataTX, pinConnected] = setRxTX(200);
+            if (pinConnected) {
                 this.appendagesConnected[0] = true;
                 // karaoke code
             }
             else {
                 this.appendagesConnected[0] = false;
-                dataRX = 0;
-                dataTX = 0;
+                dataRX = undefined;
+                dataTX = undefined;
+                pinConnected = false;
             }
         }
     }
@@ -75,36 +55,17 @@ class mainBody extends microBit {
     // appendageID 1, analogIn 400
     readDisplayAppendage() {
         while (true) {
-            let dataRX: number = 0;
-            let dataTX: number = 0;
-            if (pins.analogReadPin(AnalogPin.P0) == 400) {
-                dataRX = 10;
-                dataTX = 15;
-            }
-            else if (pins.analogReadPin(AnalogPin.P1) == 400) {
-                dataRX = 11;
-                dataTX = 16;
-            }
-            else if (pins.analogReadPin(AnalogPin.P2) == 400) {
-                dataRX = 12;
-                dataTX = 17;
-            }
-            else if (pins.analogReadPin(AnalogPin.P3) == 400) {
-                dataRX = 13;
-                dataTX = 18;
-            }
-            else if (pins.analogReadPin(AnalogPin.P4) == 400) {
-                dataRX = 14;
-                dataTX = 19;
-            }
-            if (dataRX || dataTX) {
+            // dataRX and dataTX are of type DigitalPin
+            let [dataRX, dataTX, pinConnected] = setRxTX(400);
+            if (pinConnected) {
                 this.appendagesConnected[1] = true;
                 // karaoke code
             }
             else {
                 this.appendagesConnected[1] = false;
-                dataRX = 0;
-                dataTX = 0;
+                dataRX = undefined;
+                dataTX = undefined;
+                pinConnected = false;
             }
         }
     }
@@ -112,36 +73,16 @@ class mainBody extends microBit {
     // appendageID 2, analogIn 600
     readWheelAppendage() {
         while (true) {
-            let dataRX: number = 0;
-            let dataTX: number = 0;;
-            if (pins.analogReadPin(AnalogPin.P0) == 600) {
-                dataRX = 10;
-                dataTX = 15;
-            }
-            else if (pins.analogReadPin(AnalogPin.P1) == 600) {
-                dataRX = 11;
-                dataTX = 16;
-            }
-            else if (pins.analogReadPin(AnalogPin.P2) == 600) {
-                dataRX = 12;
-                dataTX = 17;
-            }
-            else if (pins.analogReadPin(AnalogPin.P3) == 600) {
-                dataRX = 13;
-                dataTX = 18;
-            }
-            else if (pins.analogReadPin(AnalogPin.P4) == 600) {
-                dataRX = 14;
-                dataTX = 19;
-            }
-            if (dataRX || dataTX) {
+            let [dataRX, dataTX, pinConnected] = setRxTX(600);
+            if (pinConnected) {
                 this.appendagesConnected[2] = true;
                 // karaoke code
             }
             else {
                 this.appendagesConnected[2] = false;
-                dataRX = 0;
-                dataTX = 0;
+                dataRX = undefined;
+                dataTX = undefined;
+                pinConnected = false;
             }
         }
     }
@@ -149,38 +90,41 @@ class mainBody extends microBit {
     // appendageID 3, analogIn 800
     readMusicBlockAppendage() {
         while (true) {
-            let dataRX: number = 0;
-            let dataTX: number = 0;;
-            if (pins.analogReadPin(AnalogPin.P0) == 800) {
-                dataRX = 10;
-                dataTX = 15;
-            }
-            else if (pins.analogReadPin(AnalogPin.P1) == 800) {
-                dataRX = 11;
-                dataTX = 16;
-            }
-            else if (pins.analogReadPin(AnalogPin.P2) == 800) {
-                dataRX = 12;
-                dataTX = 17;
-            }
-            else if (pins.analogReadPin(AnalogPin.P3) == 800) {
-                dataRX = 13;
-                dataTX = 18;
-            }
-            else if (pins.analogReadPin(AnalogPin.P4) == 800) {
-                dataRX = 14;
-                dataTX = 19;
-            }
-            if (dataRX || dataTX) {
+            let [dataRX, dataTX, pinConnected] = setRxTX(800);
+            if (pinConnected) {
                 this.appendagesConnected[3] = true;
                 // karaoke code
             }
             else {
                 this.appendagesConnected[3] = false;
-                dataRX = 0;
-                dataTX = 0;
+                dataRX = undefined;
+                dataTX = undefined;
+                pinConnected = false;
             }
         }
+    }
+}
+
+// sets the pins for RxTx pair depending on which part of the body they are on
+// the AnalogPin for appendageID lets us know which pin the RxTX pairs are
+function setRxTX (analogInValue : number) : [SerialPin | undefined, SerialPin | undefined, boolean] {
+    // check all appendageIDPins
+    if (pins.analogReadPin(AnalogPin.P5) == analogInValue) {
+        return [SerialPin.P0, SerialPin.P12, true];
+    }
+    else if (pins.analogReadPin(AnalogPin.P6) == analogInValue) {
+        return [SerialPin.P1, SerialPin.P13, true];
+    }
+    else if (pins.analogReadPin(AnalogPin.P7) == analogInValue) {
+        return [SerialPin.P2, SerialPin.P14, true];
+    }
+    else if (pins.analogReadPin(AnalogPin.P9) == analogInValue) {
+        return [SerialPin.P8, SerialPin.P15, true];
+    }
+    // if no appendageID signal
+    // there is no data-pin to be checked
+    else {
+        return [undefined, undefined, false];
     }
 }
 
